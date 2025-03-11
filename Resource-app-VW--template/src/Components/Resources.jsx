91d82226b7a711ef6044f2cd8b6d7430db1fe18d
Victorwil <143075655+Victorwil@users.noreply.github.com>
2025-03-11 12:40:37 +0100

    endringer i resources.jsx og index.html

--- Resource-app-VW--template/src/Components/Resources.jsx
+++ Resource-app-VW--template/src/Components/Resources.jsx
@@ -4,24 +4,27 @@ import { resources } from "./ressurser.js"
 const Resources = ({ category }) => {
     console.log(" Recived Category:", category);
 
-    const filteredResources = resources.filter(res => res.category === category);
+    const mappedcategory = category === "sanity" ? "headless-cms": category;
+
+    const filteredResources = resources.filter(res => res.category === mappedcategory);
 
     console.log("filteredResources:", filteredResources)
 
 
-    if (!filteredResources || !filteredResources.sources || filteredResources.sources.length === 0) {
+    
+    if (filteredResources.length === 0) {
         return <div>Ingen ressurser tilgjengelig for denne kategorien.</div>;
     }
-    
+
 
     return (
         <div>
              <h2>{category.toUpperCase()} Resources</h2>
             <ul>
-                {filteredResources.sources.map((source, index) => (
-                <li key={index}>
-                    <a href={source.url} target ={"_blank"} rel="noopener noreferrer">
-                        {source.title}
+                {filteredResources.map((res) => (
+                <li key={res.url}>
+                    <a href={res.url} target ={"_blank"} rel="noopener noreferrer">
+                        {res.title}
                     </a>
                 </li>                    
                 ))}
